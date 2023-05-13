074b1a1780f035801d6ccaea9ff17e55ea254852
Mee <rafaleto@gmail.com>
2023-05-13 10:21:01 +0200

    refactor: Added missing "get value from context"'s

--- lifetracking/graph/Node_int.py
+++ lifetracking/graph/Node_int.py
@@ -40,45 +40,31 @@ class Node_int_generate(Node_int):
 
 
 class Node_int_singleincrement(Node_int):
-    def __init__(self, node: Node_int) -> None:
-        self.node = node
+    def __init__(self, n0: Node_int) -> None:
+        self.n0 = n0
 
-    def _operation(self, node, t=None) -> int:
+    def _operation(self, n0, t=None) -> int:
         time.sleep(t or 0)
-        return node + 1
+        return n0 + 1
 
     def _run_sequential(self, t=None, context=None) -> int | None:
-        # Depending on the context, the node can be already computed!
-        node_output = None
-        if context is not None and self.node in context:
-            node_output = context[self.node]
-        else:
-            node_output = self.node._run_sequential(t, context)
-        if context is not None:
-            context[self.node] = node_output
-
-        # The operation is performed
+        # Node is calculated if it's not in the context, then _operation is called
+        n0_out = self._get_value_from_context_or_run(self.n0, t, context)
         return self._operation(
-            node_output,
+            n0_out,
             t,
         )
 
     def _make_prefect_graph(self, t=None, context=None) -> PrefectFuture:
-        node_output = None
-        if context is not None and self.node in context:
-            node_output = context[self.node]
-        else:
-            node_output = self.node._make_prefect_graph(t, context)
-        if context is not None:
-            context[self.node] = node_output
-
-        # The operation is performed
+        # Node graph is calculated if it's not in the context, then _operation is called
+        n0_out = self._get_value_from_context_or_makegraph(self.n0, t, context)
         return prefect_task(name=self.__class__.__name__)(self._operation).submit(
-            node_output, t
+            n0_out,
+            t,
         )
 
     def _get_children(self) -> set[Node]:
-        return {self.node}
+        return {self.n0}
 
 
 class Node_int_addition(Node_int):
@@ -91,10 +77,9 @@ class Node_int_addition(Node_int):
         return n0 + n1
 
     def _run_sequential(self, t=None, context=None) -> int | None:
+        # Node is calculated if it's not in the context, then _operation is called
         n0_out = self._get_value_from_context_or_run(self.n0, t, context)
         n1_out = self._get_value_from_context_or_run(self.n1, t, context)
-
-        # The operation is performed
         return self._operation(
             n0_out,
             n1_out,
@@ -102,9 +87,9 @@ class Node_int_addition(Node_int):
         )
 
     def _make_prefect_graph(self, t=None, context=None) -> PrefectFuture:
+        # Node graph is calculated if it's not in the context, then _operation is called
         n0_out = self._get_value_from_context_or_makegraph(self.n0, t, context)
         n1_out = self._get_value_from_context_or_makegraph(self.n1, t, context)
-
         return prefect_task(name=self.__class__.__name__)(self._operation).submit(
             n0_out,
             n1_out,
@@ -112,4 +97,4 @@ class Node_int_addition(Node_int):
         )
 
     def _get_children(self) -> set[Node]:
-        return {self.node1, self.node2}
+        return {self.n0, self.n1}
