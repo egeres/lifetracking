4b88bf619c9fe8316913284ae87ebb006eaf021c
egeres <egeres.default@gmail.com>
2023-05-13 21:19:45 +0200

    refactor: Minor tweaks to `update_version.py`

--- tools/update_version.py
+++ tools/update_version.py
@@ -6,7 +6,7 @@ from tomlkit import dumps, parse
 
 def get_latest_git_tag():
     result = subprocess.run(
-        ["git", "describe", "--abbrev=0", "--tags"], capture_output=True, text=True
+        args=["git", "describe", "--abbrev=0", "--tags"], capture_output=True, text=True
     )
     if result.returncode != 0:
         raise Exception("Failed to fetch latest Git tag.")
@@ -19,7 +19,7 @@ def update_version_in_pyproject_toml(version):
 
     if version.startswith("v"):
         version = version[1:]
-    content["tool"]["poetry"]["version"] = version
+    content["tool"]["poetry"]["version"] = version  # type: ignore
 
     with open("pyproject.toml", "w") as f:
         f.write(dumps(content))
@@ -29,7 +29,8 @@ def main():
     latest_tag = get_latest_git_tag()
     if not re.match(r"^v?\d+\.\d+\.\d+$", latest_tag):
         raise Exception(
-            f"Invalid Git tag: {latest_tag}. Must be in semantic versioning format (e.g., 1.2.3)"
+            f"Invalid Git tag: {latest_tag}."
+            + "Must be in semantic versioning format (e.g., 1.2.3)"
         )
 
     update_version_in_pyproject_toml(latest_tag)
