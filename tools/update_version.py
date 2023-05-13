98cdefbe04ec6878f3335e880907db1e97d4285f
egeres <egeres.default@gmail.com>
2023-05-13 21:11:44 +0200

    feat: Added auto-version update in pre-commits

---
+++ tools/update_version.py
@@ -0,0 +1,40 @@
+import re
+import subprocess
+
+from tomlkit import dumps, parse
+
+
+def get_latest_git_tag():
+    result = subprocess.run(
+        ["git", "describe", "--abbrev=0", "--tags"], capture_output=True, text=True
+    )
+    if result.returncode != 0:
+        raise Exception("Failed to fetch latest Git tag.")
+    return result.stdout.strip()
+
+
+def update_version_in_pyproject_toml(version):
+    with open("pyproject.toml") as f:
+        content = parse(f.read())
+
+    if version.startswith("v"):
+        version = version[1:]
+    content["tool"]["poetry"]["version"] = version
+
+    with open("pyproject.toml", "w") as f:
+        f.write(dumps(content))
+
+
+def main():
+    latest_tag = get_latest_git_tag()
+    if not re.match(r"^v?\d+\.\d+\.\d+$", latest_tag):
+        raise Exception(
+            f"Invalid Git tag: {latest_tag}. Must be in semantic versioning format (e.g., 1.2.3)"
+        )
+
+    update_version_in_pyproject_toml(latest_tag)
+    print(f"Updated version in pyproject.toml to {latest_tag}")
+
+
+if __name__ == "__main__":
+    main()
