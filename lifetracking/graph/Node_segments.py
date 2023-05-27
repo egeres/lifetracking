fa65ad32036702a2cb9c64165b062ff6df4aa0d4
egeres <egeres.default@gmail.com>
2023-05-27 15:35:19 +0200

    feat: Added first version of `Node_segmentize_pandas`

--- lifetracking/graph/Node_segments.py
+++ lifetracking/graph/Node_segments.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime
 import hashlib
 from typing import Any
 
@@ -8,6 +9,7 @@ from prefect import task as prefect_task
 from prefect.futures import PrefectFuture
 from prefect.utilities.asyncutils import Sync
 
+from lifetracking.datatypes.Seg import Seg
 from lifetracking.datatypes.Segment import Segments
 from lifetracking.graph.Node import Node
 from lifetracking.graph.Node_pandas import Node_pandas
@@ -93,4 +95,79 @@ class Node_segments_from_pdDataframe(Node_segments):
         return prefect_task(name=self.__class__.__name__)(self._operation).submit(
             n0_out,
             t,
+        )
+
+
+class Node_segmentize_pandas(Node_segments):
+    def __init__(
+        self,
+        n0: Node_pandas,
+        config: Any,
+        time_column_name: str,
+    ) -> None:
+        # assert isinstance(n0, Node_pandas)
+        super().__init__()
+        self.n0 = n0
+        self.config = config
+        self.time_column_name = time_column_name
+
+    def _get_children(self) -> list[Node]:
+        return [self.n0]
+
+    def _hashstr(self) -> str:
+        return super()._hashstr()
+
+    def _operation(
+        self,
+        n0: pd.DataFrame | PrefectFuture[pd.DataFrame, Sync],
+        t: Time_interval | None = None,
+    ) -> list[tuple[datetime.datetime, datetime.datetime]]:
+        assert t is None or isinstance(t, Time_interval)
+
+        # Variable loading
+        column_to_process = self.config[0]
+        values_of_interest = self.config[1]
+        time_to_split_in_mins = 5.0
+        df: pd.DataFrame = n0  # type: ignore
+        df[self.time_column_name] = pd.to_datetime(
+            df[self.time_column_name], format="ISO8601"
+        )
+
+        # Filtering
+        df = df[df[column_to_process].isin(values_of_interest)]
+
+        # Segmentizing
+        to_return = []
+        time_delta = pd.Timedelta(minutes=time_to_split_in_mins)
+        if not df.empty:
+            start_time = df.iloc[0][self.time_column_name]
+            end_time = df.iloc[0][self.time_column_name]
+
+            for i in range(1, len(df)):
+                current_time = df.iloc[i][self.time_column_name]
+                if (current_time - end_time) > time_delta:
+                    to_return.append(Seg(start_time, end_time))
+                    start_time = current_time
+                end_time = current_time
+
+            to_return.append(Seg(start_time, end_time))
+
+        return Segments(to_return)
+
+    def _run_sequential(
+        self, t: Time_interval | None = None, context: dict[Node, Any] | None = None
+    ) -> Segments | None:
+        n0_out = self._get_value_from_context_or_run(self.n0, t, context)
+        if n0_out is None:
+            return None
+        return self._operation(n0_out, t)
+
+    def _make_prefect_graph(
+        self, t: Time_interval | None = None, context: dict[Node, Any] | None = None
+    ) -> PrefectFuture[Segments, Sync] | None:
+        n0_out = self._get_value_from_context_or_makegraph(self.n0, t, context)
+        if n0_out is None:
+            return None
+        return prefect_task(name=self.__class__.__name__)(self._operation).submit(
+            n0_out, t
         )
