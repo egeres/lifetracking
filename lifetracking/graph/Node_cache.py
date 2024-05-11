624fd0a040fb79df90e44d6945e0cdc184e93fb0
egeres <egeres.default@gmail.com>
2024-05-11 10:35:07 +0200

    refactor: Removed redundant code from `Node_cache`, more `Path`

--- lifetracking/graph/Node_cache.py
+++ lifetracking/graph/Node_cache.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import copy
-import datetime
 import json
 import os
 import pickle
+import tempfile
+from datetime import datetime
 from functools import reduce
 from pathlib import Path
 from typing import Any, Callable, TypeVar
@@ -21,7 +22,7 @@ T = TypeVar("T")
 
 
 # REFACTOR: Remove the .strftime("%Y-%m-%d %H:%M:%S") thing
-# TODO_1:
+# TODO_1: all .now() should include timezone I guess
 
 
 class Node_cache(Node[T]):
@@ -44,13 +45,20 @@ class Node_cache(Node[T]):
             raise TypeError(msg)
 
         super().__init__()
+
         self.n0 = n0
         self.resolution = resolution
         self.days_to_expire = days_to_expire
-        # TODO_1: Add a method that gets a default temporary directory
-        self.path_dir_caches = path_dir_caches or r"C:\Temp\lifetracking\caches"
-        if not os.path.isdir(self.path_dir_caches):
-            os.makedirs(self.path_dir_caches)
+
+        hash_node = self.hash_tree()
+        if self.name is not None:
+            hash_node += "_" + self.name.replace(" ", "-")
+
+        path_dir_caches = path_dir_caches or tempfile.gettempdir()
+        if isinstance(path_dir_caches, str):
+            path_dir_caches = Path(path_dir_caches)
+        self.path_dir_caches = path_dir_caches / hash_node
+        self.path_dir_caches.mkdir(parents=True, exist_ok=True)
 
     def _get_children(self) -> list[Node]:
         return [self.n0]
@@ -69,30 +77,31 @@ class Node_cache(Node[T]):
         hash_node = self.hash_tree()
         if self.name is not None:
             hash_node += "_" + self.name.replace(" ", "-")
-        path_dir_cache = os.path.join(self.path_dir_caches, hash_node)
-        if not os.path.isdir(path_dir_cache):
-            os.makedirs(path_dir_cache)
 
-        # Cache validation
-        path_fil_cache = os.path.join(path_dir_cache, "cache.json")
-        cache_is_valid = self._is_cache_valid(path_fil_cache, hash_node, t)
+        # assert isinstance(self.path_dir_caches, Path)  # TODO_2: Remove in the future
+        dir_cache = self.path_dir_caches / hash_node
+        # dir_cache.mkdir(parents=True, exist_ok=True)
 
         # We have nothing? -> Make cache
-        if not cache_is_valid:
+        # We have something? -> Load what we can and recompute anything missing
+
+        # TODO_2: Actually, it's pretty dumb to pass
+        # - dir_cache since it's self.path_dir_caches / hash_node
+        # Like, why not get it via self.path_dir_caches / self.hash_tree()?
+
+        if not self._is_cache_valid(hash_node, t):
             return self._save_cache(
                 t,
                 n0,
-                path_dir_cache,
+                # str(dir_cache),
                 hash_node,
                 context,
                 prefect,
             )
-
-        # We have something? -> Load what we can and recompute anything missing
         return self._load_cache(
             t,
             n0,
-            path_dir_cache,
+            # str(dir_cache),
             hash_node,
             context,
             prefect,
@@ -104,46 +113,47 @@ class Node_cache(Node[T]):
     def _make_prefect_graph(self, t=None, context=None) -> PrefectFuture[T, Sync]:
         return self._operation(self.n0, t, context, prefect=True)
 
-    def _is_cache_valid(
-        self,
-        path_fil_cache: str,
-        hash_node: str,
-        t: Time_interval | None,
-    ) -> bool:
+    # Actual custom methods
+
+    def _is_cache_valid(self, hash_node: str, t: Time_interval | None) -> bool:
         """Can we use this cache?"""
 
         assert self.resolution is not None
-        assert isinstance(path_fil_cache, str)
         assert isinstance(hash_node, str)
         assert isinstance(t, Time_interval) or t is None
 
-        if not os.path.exists(path_fil_cache):
+        file_cache = self.path_dir_caches / "cache.json"
+
+        if not file_cache.exists():
             return False
+
+        # Load data
         try:
-            with open(path_fil_cache) as f:
+            with open(file_cache) as f:
                 cache_info = json.load(f)
-
-                # Is the right resolution
-                if Time_resolution(cache_info["resolution"]) != self.resolution:
-                    return False
-
-                # Is the right hash
-                if cache_info["hash_node"] != hash_node:
-                    return False
-
-                # Hasn't expired
-                if (
-                    datetime.datetime.now()
-                    - datetime.datetime.fromisoformat(cache_info["date_creation"])
-                ).days > self.days_to_expire:
-                    return False
-
-                # Cache is a slice, but they ask for the full data
-                if cache_info["type"] == "slice" and t is None:
-                    return True
-
         except Exception:
             return False
+
+        if not isinstance(cache_info, dict):
+            return False
+
+        # Is the right resolution
+        if Time_resolution(cache_info["resolution"]) != self.resolution:
+            return False
+
+        # Is the right hash
+        if cache_info["hash_node"] != hash_node:
+            return False
+
+        # Hasn't expired
+        if (
+            datetime.now() - datetime.fromisoformat(cache_info["date_creation"])
+        ).days > self.days_to_expire:
+            return False
+
+        # Cache is a slice, but they ask for the full data
+        if cache_info["type"] == "slice" and t is None:
+            return True
 
         return True
 
@@ -151,17 +161,19 @@ class Node_cache(Node[T]):
         self,
         t: Time_interval | None,
         n0: Node,
-        path_dir_cache: str,
         hash_node: str,
         context: dict | None = None,
         prefect: bool = False,
     ):
-        path_fil_cache = os.path.join(path_dir_cache, "cache.json")
+        assert isinstance(hash_node, str)
+        assert isinstance(prefect, bool)
+
+        path_fil_cache = self.path_dir_caches / "cache.json"
 
         cache_info = {
             "resolution": self.resolution.value,
             "hash_node": hash_node,
-            "date_creation": datetime.datetime.now().isoformat(),
+            "date_creation": datetime.now().isoformat(),
             "start": None,
             "end": None,
             "data": {},  # Per-resolution-data
@@ -202,7 +214,7 @@ class Node_cache(Node[T]):
                     current_day = aa.start.replace(
                         hour=0, minute=0, second=0, microsecond=0
                     ).isoformat()
-                    current_data["creation_time"] = datetime.datetime.now().isoformat()
+                    current_data["creation_time"] = datetime.now().isoformat()
                     current_data["data_count"] = 1
                     data_to_save.append(aa)
                     continue
@@ -218,8 +230,8 @@ class Node_cache(Node[T]):
                     continue
 
                 # Saving data in pickle & metadata
-                filename_slice = os.path.join(
-                    path_dir_cache, f"{current_day.split('T')[0]}.pickle"
+                filename_slice = (
+                    self.path_dir_caches / f"{current_day.split('T')[0]}.pickle"
                 )
                 with open(filename_slice, "wb") as f:
                     pickle.dump(type(o)(data_to_save), f)
@@ -231,15 +243,15 @@ class Node_cache(Node[T]):
                 ).isoformat()
                 data_to_save = [aa]
                 current_data = {
-                    "creation_time": datetime.datetime.now().isoformat(),
+                    "creation_time": datetime.now().isoformat(),
                     "data_count": 1,
                 }
 
             # "After for"
             if len(data_to_save) != 0:
                 # Saving data in pickle & metadata
-                filename_slice = os.path.join(
-                    path_dir_cache, f"{current_day.split('T')[0]}.pickle"
+                filename_slice = (
+                    self.path_dir_caches / f"{current_day.split('T')[0]}.pickle"
                 )
                 with open(filename_slice, "wb") as f:
                     pickle.dump(type(o)(data_to_save), f)
@@ -345,7 +357,7 @@ class Node_cache(Node[T]):
                             hour=0, minute=0, second=0, microsecond=0
                         ).isoformat()
                     ] = {
-                        "creation_time": datetime.datetime.now().isoformat(),
+                        "creation_time": datetime.now().isoformat(),
                         "data_count": len(o),
                     }
 
@@ -374,7 +386,7 @@ class Node_cache(Node[T]):
         if len(pre) > 0:
             start_time = cache_info["start"]
             if isinstance(start_time, str):
-                start_time = datetime.datetime.fromisoformat(start_time)
+                start_time = datetime.fromisoformat(start_time)
             min_pre = min(pre)
             if isinstance(min_pre, pd.Timestamp):
                 min_pre = min_pre.to_pydatetime()
@@ -384,7 +396,7 @@ class Node_cache(Node[T]):
         if len(pre) > 0:
             time_end = cache_info["end"]
             if isinstance(time_end, str):
-                time_end = datetime.datetime.fromisoformat(time_end)
+                time_end = datetime.fromisoformat(time_end)
             max_pre = max(pre)
             if isinstance(max_pre, pd.Timestamp):
                 max_pre = max_pre.to_pydatetime()
@@ -400,32 +412,34 @@ class Node_cache(Node[T]):
         self,
         t: Time_interval | None,
         n0: Node,
-        path_dir_cache: str,
         hash_node: str,
         context: dict | None = None,
         prefect: bool = False,
     ) -> T | None:
+
+        assert isinstance(hash_node, str)
+        assert isinstance(prefect, bool)
+
         to_return: list[T] = []
 
         # Cache data loading
-        path_fil_cache = os.path.join(path_dir_cache, "cache.json")
-        with open(path_fil_cache) as f:
-            cache_info = json.load(f)
+        path_fil_cache = self.path_dir_caches / "cache.json"
+        cache_info = json.loads(path_fil_cache.read_text())
         t_cache = Time_interval(
-            datetime.datetime.fromisoformat(cache_info["start"]),
-            datetime.datetime.fromisoformat(cache_info["end"]),
+            datetime.fromisoformat(cache_info["start"]),
+            datetime.fromisoformat(cache_info["end"]),
         )
 
         if t is None and cache_info["type"] == "slice":
             to_return += self._gather_existing_slices(
                 [t_cache],
-                path_dir_cache,
+                str(self.path_dir_caches),
             )
 
             o_0 = n0._run_sequential(
                 Time_interval(
-                    datetime.datetime.min,
-                    datetime.datetime.fromisoformat(cache_info["start"]),
+                    datetime.min,
+                    datetime.fromisoformat(cache_info["start"]),
                 ),
                 context,
             )
@@ -433,8 +447,8 @@ class Node_cache(Node[T]):
                 to_return.append(o_0)
             o_1 = n0._run_sequential(
                 Time_interval(
-                    datetime.datetime.fromisoformat(cache_info["end"]),
-                    datetime.datetime.max,
+                    datetime.fromisoformat(cache_info["end"]),
+                    datetime.max,
                 ),
                 context,
             )
@@ -448,18 +462,18 @@ class Node_cache(Node[T]):
 
             to_return += self._gather_existing_slices(
                 slices_to_get,
-                path_dir_cache,
+                str(self.path_dir_caches),
             )
             to_return += self._compute_nonexistent_slices(
                 slices_to_compute,
-                path_dir_cache,
+                str(self.path_dir_caches),
                 prefect,
                 n0,
                 cache_info,
                 context,
             )
 
-        self._update_metadata_bounds(to_return, path_dir_cache, t)
+        self._update_metadata_bounds(to_return, str(self.path_dir_caches), t)
 
         if len(to_return) == 0:
             return None
