d4e209d4ab49acf891b7fd86cfd6f1fc94a5555a
egeres <egeres.default@gmail.com>
2024-07-20 19:43:31 +0200

    fix: Mini issues with previous commit

--- lifetracking/dataloggers/single.py
+++ lifetracking/dataloggers/single.py
@@ -30,14 +30,14 @@ def get_system_details(way_this_info_was_added: str) -> dict[str, str]:
 
 def write_single_on_csv(
     path_to_file: Path,
-    way_this_info_was_added: str = "manual",
     now: datetime | None = None,
+    way_this_info_was_added: str = "manual",
     **kwargs,
 ):
     assert isinstance(path_to_file, Path)
+    assert isinstance(now, datetime) or now is None
     assert path_to_file.suffix == ".csv"
     assert isinstance(way_this_info_was_added, str)
-    assert isinstance(now, datetime) or now is None
 
     now = datetime.now(timezone.utc) if now is None else now
     d = get_system_details(way_this_info_was_added)
@@ -46,32 +46,30 @@ def write_single_on_csv(
     with path_to_file.open(mode, encoding="utf-8", newline="") as f:
         # TODO_2: Add check if columns change from the header to what it's being saved
 
-        writer = csv.writer(f)
+        w = csv.writer(f)
         if mode == "w":
-            writer.writerow(["datetime", *d.keys(), *kwargs.keys()])
-        writer.writerow([now, *d.values(), *[str(x) for x in kwargs.values()]])
+            w.writerow(["datetime", *d.keys(), *kwargs.keys()])
+        w.writerow([now.isoformat(), *d.values(), *[str(x) for x in kwargs.values()]])
 
 
 def write_single_on_dated_json(
     path_to_dir: Path,
-    way_this_info_was_added: str = "manual",
     now: datetime | None = None,
+    way_this_info_was_added: str = "manual",
     **kwargs,
 ):
     assert isinstance(path_to_dir, Path)
+    assert isinstance(now, datetime) or now is None
     assert path_to_dir.is_dir()
     assert isinstance(way_this_info_was_added, str)
-    assert isinstance(now, datetime) or now is None
 
-    if now is None:
-        now = datetime.now(timezone.utc)
+    now = datetime.now(timezone.utc) if now is None else now
 
     fil = path_to_dir / f"{now.strftime('%Y-%m-%d')}.json"
-    df: list[dict] = [] if fil.exists() else json.load(fil.open("r", encoding="utf-8"))
+    df: list[dict] = json.load(fil.open("r", encoding="utf-8")) if fil.exists() else []
     df.append(
         {"datetime": now.isoformat()}
         | get_system_details(way_this_info_was_added)
         | kwargs
     )
-    with fil.open("w", encoding="utf-8") as f:
-        json.dump(df, f, indent=4)
+    json.dump(df, fil.open("w", encoding="utf-8"), indent=4)
