16a904b358d6057c7992fb69387555bc110ba178
egeres <egeres.default@gmail.com>
2024-07-27 01:39:33 +0200

    fix: Broken test

--- tests/test_node_type_geopandas.py
+++ tests/test_node_type_geopandas.py
@@ -1,10 +1,8 @@
 import pytest
 
-from lifetracking.graph.Node_geopandas import (
-    Reader_geojson,
-)
+from lifetracking.graph.Node_geopandas import Reader_geodata
 
 
 def test_node_geopandas_creation():
     with pytest.raises(AssertionError):
-        Reader_geojson("Some path")
+        Reader_geodata("Some path")
